dabbdcffa00791724dd7bedfd8f08bc7ef76d090
Joey Chance <joey.chance@u.nus.edu>
2023-02-16 23:40:05 +0800

    Implement priority queue

--- engine.cpp
+++ engine.cpp
@@ -6,36 +6,57 @@
 #include <utility>
 #include <unordered_set>
 #include <functional>
+#include <set>
 
 #include "io.hpp"
 #include "engine.hpp"
 
+struct Order
+{
+	ClientCommand info;
+	uint64_t time;
+	bool operator==(const Order &other) const
+	{
+		return info.order_id == other.info.order_id;
+	}
+	bool operator<(const Order& a) const
+	{
+		if (info.price == a.info.price) 
+		{
+			return a.time > time;
+		}
+		return a.info.price < info.price;
+	}
+	bool operator>(const Order& a) const
+	{
+		if (info.price == a.info.price) 
+		{
+			return a.time > time;
+		}
+		return a.info.price > info.price;
+	}
+};
 
-struct BuyList {
-	auto cmp = [](ClientCommand& a, ClientCommand& b) {
-		//TODO: implement comparator
-		return true;
-	};
-	std::set<ClientCommand&, cmp> orders;
+struct Buys
+{
+	std::set<Order> pq;
 	std::mutex match_mutex;
 	std::mutex execute_mutex;
 	std::mutex enqueue_mutex;
 };
 
-struct SellList {
-	auto cmp = [](ClientCommand& a, ClientCommand& b) {
-		//TODO: implement comparator
-		return true;
-	};
-	std::set<ClientCommand&, cmp> orders;
+struct Sells
+{
+	std::set<Order, std::greater<Order> > pq;
 	std::mutex match_mutex;
 	std::mutex execute_mutex;
 	std::mutex enqueue_mutex;
 };
 
-using Orders = std::pair<BuyList, SellList>;
+using Orders = std::pair<Buys, Sells>;
 
-static size_t order_id = 0;
+static uint64_t timestamp = 0;
+static uint32_t order_id = 0;
 static std::unordered_map<std::string, Orders> order_book;
 
 void Engine::accept(ClientConnection connection)
@@ -46,8 +67,7 @@ void Engine::accept(ClientConnection connection)
 
 void Engine::connection_thread(ClientConnection connection)
 {
-	std::unordered_map<int, std::weak_ptr<ClientCommand>> my_orders();
-	//std::unordered_map<int, iter of order_book> my_orders();
+	std::unordered_map<int, std::set<Order>& > my_orders;
 	while(true)
 	{
 		ClientCommand input {};
@@ -76,8 +96,6 @@ void Engine::connection_thread(ClientConnection connection)
 				break;
 			}
 			case input_buy: {
-				
-				my_orders[input.order_id];
 				break;
 			}
 			case input_sell: {
@@ -108,3 +126,33 @@ void Engine::connection_thread(ClientConnection connection)
 		Output::OrderExecuted(123, 124, 1, 2000, 10, output_time);
 	}
 }
+
+
+// Testing priority queue
+/*
+ClientCommand input {};
+	connection.readInput(input);
+	ClientCommand input1 {input_buy, order_id++, 5, 2, "AAPL"};
+	Order order1 {input1, 3};
+	ClientCommand input2 {input_buy, order_id++, 1, 1, "AAPL"};
+	Order order2 {input2, 3};
+	ClientCommand input3 {input_buy, order_id++, 1, 3, "AAPL"};
+	Order order3 {input3, 2};
+	ClientCommand input4 {input_buy, order_id++, 5, 4, "AAPL"};
+	Order order4 {input4, 1};
+	Buys buys;
+	buys.pq.insert(order1);
+	buys.pq.insert(order2);
+	buys.pq.insert(order3);
+	buys.pq.insert(order4);
+	Order top = *buys.pq.begin();
+	std::cout << top.info.price << top.info.count << std::endl;
+	Sells sells;
+	sells.pq.insert(order2);
+	sells.pq.insert(order1);
+	sells.pq.insert(order3);
+	sells.pq.insert(order4);
+	Order bottom = *sells.pq.begin();
+	std::cout << bottom.info.price << bottom.info.count << std::endl;
+	return;
+*/
