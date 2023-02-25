2642abe13e0a8df1c2b553e867a9dc6b5c13685b
blimyj <blimyj21@gmail.com>
2023-02-25 14:32:03 +0800

    Modify custom runner to run with std::threads and std::barrier sync

--- scripts/custom_runner/custom_runner.cpp
+++ scripts/custom_runner/custom_runner.cpp
@@ -11,7 +11,13 @@
 #include <sys/socket.h>
 
 #include <atomic>
+
+#include <thread>
 #include <fstream>
+#include <vector>
+#include <barrier>
+#include <functional>
+#include <typeinfo>
 
 #include "io.hpp"
 
@@ -19,8 +25,6 @@
 #define INPUT_BUY_ORDER 'B'
 #define INPUT_SELL_ORDER 'S'
 
-static char* line_buffer;
-static size_t line_buffer_size = 0;
 static std::atomic<bool> main_is_exiting = 0;
 
 static void* poll_thread(void* fdptr)
@@ -49,8 +53,13 @@ static void* poll_thread(void* fdptr)
 	return 0;
 }
 
-static void* run_client(void *sock_path)
+//Function objects are so hard T.T 
+//https://stackoverflow.com/questions/75197002/how-to-pass-an-stdbarrier-with-a-lambda-completion-function-to-a-named-functio
+//Completion Function type here
+void* run_client(void *sock_path, std::string input_fn, std::barrier<void(*)(void) noexcept>& sync_point)
 {
+    char* line_buffer;
+    size_t line_buffer_size = 0;
     // Get char * from void*
     char *socket_path = (char*)sock_path;
 
@@ -58,6 +67,8 @@ static void* run_client(void *sock_path)
     void *exit_0 = 0;
     void *exit_1 = (void*) 1;
 
+    //Sync with other threads before proceeding
+    sync_point.arrive_and_wait();
 
     //Create conn to engine
 	int clientfd = socket(AF_UNIX, SOCK_STREAM, 0);
@@ -90,7 +101,8 @@ static void* run_client(void *sock_path)
 		pthread_exit(exit_1);//return 1;
 	}
 
-    FILE *input_file = fopen("c1.in", "r");
+    // FILE *input_file = fopen("c1.in", "r");
+    FILE *input_file = fopen(input_fn.c_str(), "r");
 	while(1)
 	{
 		ClientCommand input {};
@@ -122,6 +134,7 @@ static void* run_client(void *sock_path)
 					fprintf(stderr, "Invalid new order: %s\n", line_buffer);
 					pthread_exit(exit_1);//return 1;
 				}
+                //fprintf(stdout, "%c %u %8s %u %u\n", input.type, input.order_id, input.instrument, input.price, input.count);
 				break;
 			default: fprintf(stderr, "Invalid command '%c'\n", line_buffer[0]); pthread_exit(exit_1);//return 1;
 		}
@@ -140,8 +153,7 @@ static void* run_client(void *sock_path)
 }
 
 int main(int argc, char* argv[]) {
-    //argv[1] should be socket path
-    //pthread_create(run_client, argv[1])
+    
     if(argc < 2)
 	{
 		fprintf(stderr, "Usage: %s <path of socket to connect to> < <input>\n", argv[0]);
@@ -150,8 +162,7 @@ int main(int argc, char* argv[]) {
     //Get Socket path
     char sock_path[100];
     strncpy(sock_path, argv[1], strlen(argv[1]) + 1);
-    // fprintf(stdout, "%s\n", argv[1]);
-    // fprintf(stdout, "%s end\n", sock_path);
+    
     // Get number of input files, N_Clients
     // argc -2
     // print error if less than 1
@@ -164,14 +175,34 @@ int main(int argc, char* argv[]) {
     //Create array of client threads and statuses, size of N_Clients
     //Loops through and run create/join
     const int max_threads = 40;
-    pthread_t client_threads[max_threads];
-    void *client_statuses[max_threads];
-    for (int i=0; i< N_Clients; i++) {
-        pthread_t *client_thread = &client_threads[i];
-        void *client_status = client_statuses[i];
+    std::vector<std::string> input_filenames;
+    for (int i=0; i < N_Clients; i++) {
+        input_filenames.push_back(std::string(argv[i+2]));
+        //std::cout << std::string(argv[i+2]) << std::endl;
+    }
 
-        pthread_create(client_thread, NULL, run_client, (void*) sock_path);
-        pthread_join(*client_thread, &client_status);
+    // Create sync barrier for threads
+    auto on_completion = []() noexcept
+    {
+        // locking not needed here
+        static auto phase = "Barrier Reached\n";
+        std::cout << phase;
+    };
+    std::barrier<void(*)(void) noexcept> sync_point(N_Clients, on_completion);
+    // std::barrier<> sync_point(N_Clients);
+    
+    // std::cout << type_name<decltype(on_completion)>() << '\n';
+    // std::cout << "Type of x : " << typeid(on_completion).name() << std::endl;
+
+    std::thread client_threads[max_threads];
+    for (int i=0; i< N_Clients; i++) {
+
+        client_threads[i] = std::thread(run_client, (void*) sock_path, input_filenames[i], std::ref(sync_point));
+        // client_threads[i] = std::thread(run_client, (void*) sock_path, input_filenames[i]);
+    }
+    for (int i=0; i< N_Clients; i++) {
+
+        client_threads[i].join();
     }
     
 }
